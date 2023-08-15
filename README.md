3950fc92cfd6d7b0c64cc854e2c7d34b3c93f2f8
fintafixX <140941794+fintafixX@users.noreply.github.com>
2023-08-15 11:08:54 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,114 @@
-# server119311
+# DiscordBot-BardGPT
+> ## Using Google Bard Chat AI on discord bot.
+
+## Update
+> ### 2023/7/19 : Message can contain image.
+> ### 2023/7/15 : Use another Bard api so the chatbot can work again.
+> ### 2023/6/5 : Use SQLite storage of individual cookies so it doesn't reset when the bot restarts, it can also be exported for later use.
+
+## Features
+
+<details>
+   <summary>
+   
+   ### Slash command
+
+   </summary>
+   
+> ### will create a personal conversation for each user.
+   
+* cookies setting(use personal Google Bard Cookies): `/bard_cookies [choice]`
+  
+  ![setting](https://i.imgur.com/Q5HS6SW.png)
+  
+   
+* Bard: `/bard [message] [image]`
+  
+  >**Warning** : Only up to 10 images will be displayed.
+
+  ![Bard1](https://i.imgur.com/LEmdIMI.png)
+  ![Bard2](https://i.imgur.com/q9hy2RP.png)
+
+* reset conversation: `/reset_bard_conversation `
+  
+</details>
+
+<details>
+   <summary>
+   
+   ### Mention bot
+
+   </summary>
+
+> ### same feature as the slash command, but this will reply all user messages.
+
+* Same as use `/bard`,
+
+  ![mention1](https://i.imgur.com/1PiYBi8.png)
+  ![mention2](https://i.imgur.com/Tf0dCeu.png)
+  ![reset](https://i.imgur.com/YykmbU6.png)
+
+</details>
+
+<details>
+   <summary>
+   
+   ### Prefix command (available only to bot owner)
+
+   </summary>
+ 
+ > ### bot owner setting.
+   
+ * `!bardunload [file_name_in_cogs_folder]`: Disable command from the specified file.
+ * `!bardload [file_name_in_cogs_folder]`: Enable the command from the specified file.
+ 
+   ![load & unload](https://i.imgur.com/lqqcxkd.png)
+  
+ * `!bardclean`: Empty discord_bot.log file.
+ * `!bardgetLog`: Get discord_bot.log file. Real-time tracking of the bot's operating status.
+   
+   ![bardgetLog](https://i.imgur.com/ZQok7qS.png)
+ 
+ * `!bardgetdb`: Export Bard_id.db file
+   
+    ![getdb](https://i.imgur.com/gq9E7lV.png)
+   
+ * `!bardupload [__Secure-1PSID]`: Set default __Secure-1PSID.
+ 
+   ![upload](https://i.imgur.com/3FZmmdu.png)
+   
+</details>
+
+## Install
+```
+pip install -r requirements.txt
+```
+
+## Usage
+1. Rename the file`.env.dev`to`.env`, then open it and edit it. If you don't want a limit channel to mention a bot, you don't need to set up a MENTION_CHANNEL_ID, just leave it blank.
+   ```
+   DISCORD_BOT_TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+   MENTION_CHANNEL_ID=123456789
+   ```
+   
+2. Get Google Bard authentication.
+   * Go to [https://bard.google.com/](https://bard.google.com/)
+   * F12 for console
+   * Session: Go to Application → Cookies → `__Secure-1PSID`. Copy the value of that cookie.
+
+3. Start run your bot, hosted locally or on a server.
+
+   -> Recommended Free Servers: [fly.io](https://fly.io/)
+
+4. Use `!bardupload` prefix command to set your session.
+
+## Credits
+* Bard - [https://github.com/dsdanielpark/Bard-API/tree/main](https://github.com/dsdanielpark/Bard-API/tree/main)
+
+## Contributors
+
+This project exists thanks to all the people who contribute.
+
+ <a href="https://github.com/FuseFairy/DiscordBot-BradGPT/graphs/contributors">
+  <img src="https://contrib.rocks/image?repo=FuseFairy/DiscordBot-BardGPT" />
+ </a>
---
+++ bot.py
@@ -0,0 +1,120 @@
+import discord
+import os
+import src.log
+import sqlite3
+import pkg_resources
+from src.setChatbot import update_session_id, init_sql_chatbot
+from src.setChatbot import set_chatbot
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+# init loggger
+logger = src.log.setup_logger(__name__)
+
+conn = sqlite3.connect('Bard_id.db')
+c = conn.cursor()
+try:
+    c.execute('''CREATE TABLE ID_DATA
+                 (USER_ID   INT PRIMARY KEY    NOT NULL,
+                 SECURE_1PSID   TEXT           NOT NULL,
+                 default_value  INT);''')
+    conn.commit()
+    conn.close()
+except:
+    pass
+
+def check_version():
+    required = [line.strip() for line in open('requirements.txt')]
+
+    for package in required:
+        package_name, package_version = package.split('==')
+        name, version = pkg_resources.get_distribution(package_name).project_name, pkg_resources.get_distribution(package_name).version
+        if package != f'{name}=={version}':
+            raise ValueError(f'{name} version {version} is installed but does not match the requirements')
+
+@bot.event
+async def on_ready():
+    bot_status = discord.Status.online
+    bot_activity = discord.Activity(type=discord.ActivityType.playing, name="/help")
+    await bot.change_presence(status=bot_status, activity=bot_activity)
+    await init_sql_chatbot()
+    for Filename in os.listdir('./cogs'):
+        if Filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{Filename[:-3]}')  
+    logger.info(f'{bot.user} is now running!')
+    print("Bot is Up and Ready!")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} commands")
+    except Exception as e:
+        print(e)
+
+# Load command
+@commands.is_owner()
+@bot.command(name="bardload")
+async def load(ctx, extension):
+    await bot.load_extension(f'cogs.{extension}')
+    await ctx.author.send(f'> **Loaded {extension} done.**')
+
+# Unload command
+@commands.is_owner()
+@bot.command(name="bardunload")
+async def unload(ctx, extension):
+    await bot.unload_extension(f'cogs.{extension}')
+    await ctx.author.send(f'> **Un-Loaded {extension} done.**')
+
+# Empty discord_bot.log file
+@commands.is_owner()
+@bot.command(name="bardclean")
+async def clean(ctx):
+    open('discord_bot.log', 'w').close()
+    await ctx.author.send(f'> **Successfully emptied the file!**')
+
+# Get discord_bot.log file
+@commands.is_owner()
+@bot.command(name="bardgetLog")
+async def getLog(ctx):
+    try:
+        with open('discord_bot.log', 'rb') as f:
+            file = discord.File(f)
+        await ctx.author.send(file=file)
+    except:
+        await ctx.author.send("> **Send failed!**")
+
+# Get Bard_id.db file
+@commands.is_owner()
+@bot.command(name="bardgetdb")
+async def getdb(ctx):
+    try:
+        with open('Bard_id.db', 'rb') as f:
+            file = discord.File(f)
+        await ctx.author.send(file=file)
+    except:
+        await ctx.author.send("> **Send failed!**")
+
+# Upload Bard cookies
+@commands.is_owner()
+@bot.command(name="bardupload")
+async def upload(ctx, *, message):
+    try:
+        if not isinstance(ctx.channel, discord.abc.PrivateChannel):
+            await ctx.message.delete()
+        await set_chatbot(session_id=message)
+        with sqlite3.connect('Bard_id.db') as conn:
+            c = conn.cursor()
+            c.execute("UPDATE ID_DATA SET SECURE_1PSID = ? WHERE DEFAULT_VALUE = 1", (message,))
+            conn.commit()
+        await update_session_id(new_session_id=message)
+        await ctx.author.send(f'> **Upload new \_\_Secure-1PSID successfully!**')
+        logger.warning("\x1b[31m__Secure-1PSID has been setup successfully\x1b[0m")
+    except Exception as e:
+        await ctx.author.send(f">>> **Error: {e}**")
+        logger.exception(f"Error while upload cookies: {e}")
+
+if __name__ == '__main__':
+    check_version()
+    bot.run(os.getenv("DISCORD_BOT_TOKEN"))
